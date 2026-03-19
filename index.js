8dd121920690e95bc58828890cc05ea4fb83bc93
tonghuuthang192004 <huuthang192004@gmail.com>
2026-03-19 16:06:24 +0700

    tich hop nodemon

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express') // nhúng freamwork vào 
+const app = express() // gọi hàm express 
+const port = 3000 // cổng 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// get lấy ra tạo trang chủ 
+// req gửi lên
+// res trả về 
+app.get('/', (req, res) => { 
+  res.send('123')
 })
 
+
+// khởi chạy dự án cổng 3000
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
