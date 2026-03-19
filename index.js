b9a13960336e3b7334cf8fbcf6e96e6c7e1b67a7
tonghuuthang192004 <huuthang192004@gmail.com>
2026-03-19 20:55:35 +0700

    tich hop pug

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express') // nhúng freamwork vào 
 const app = express() // gọi hàm express 
 const port = 3000 // cổng 
+const path =require('path');
+const { title } = require('process');
+app.set('views',path.join(__dirname,'./views')); // thiết lập view 
+app.set('view engine', 'pug') // chỉ định view enginner pug
 
 // get lấy ra tạo trang chủ 
 // req gửi lên
 // res trả về 
 app.get('/', (req, res) => { 
-  res.send('123')
+ res.render("client/pages/home",{pageTitle:"Trang Chủ"}) // mặc định nó đẵ vào thư mục views
 })
-
+app.get('/tours', (req, res) => { 
+  res.render('client/pages/tour',{pageTitle:"Danh sách tour"}) // mặc định nó đẵ vào thư mục views
+})
 
 // khởi chạy dự án cổng 3000
 app.listen(port, () => {
