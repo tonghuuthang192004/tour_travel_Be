bcab1b37f33fd03b7b03d676058f0329b8204913
tonghuuthang192004 <huuthang192004@gmail.com>
2026-03-27 21:06:47 +0700

    tich hop breadcrumd

--- controllers/client/tour.controller.js
+++ controllers/client/tour.controller.js
@@ -3,7 +3,7 @@ const Tour=require('../../models/tour.model');
 module.exports.tour= async (req, res)  => { 
   var tourList =  await Tour.find({});
 
-  console.log(tourList);
+  
 
   res.render('client/pages/tour',{pageTitle:"Danh sách tour",
     tourList:tourList // chạy sang giao dien view
